310fa2d99a917acedf7e78ccbc61ac8335d632ab
SINIXgo <116418436+SINIXgo@users.noreply.github.com>
2023-01-19 00:28:49 -0300

    Delete script4.lua

--- assets/preload/data/untold-loneliness/script4.lua
+++
@@ -1,5 +0,0 @@
-function onCreate() 
-makeLuaText('watermark',"Ported By Sinix Go And Mateuzinho ", 0, 2, 60); -- You can edit the created by Prisma Text just dont change anything else
-    setTextSize('watermark', 30);
-    addLuaText('watermark');
-end
